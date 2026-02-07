4768feb90b88eb43fb21fcd56034b1874b54292d
Nihit Yadav <nihit.yadav@kalvium.community>
2026-02-07 14:59:38 +0530

    firebase connection established, android studio setup completed

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -20,3 +20,28 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'SafeCare',
+//       home: Scaffold(
+//         body: Center(
+//           child: Text('Firebase Connected 🚀'),
+//         ),
+//       ),
+//     );
+//   }
+// }
